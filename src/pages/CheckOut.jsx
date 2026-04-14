6db0337f4156fab7099aa43791b4bbab8fb86fea
ayaaliabdelfattah77 <ayaali7779922@gmail.com>
2026-04-14 20:51:35 +0200

    fix imports

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,4 +1,4 @@
-import * as Yup from "Yup";
+import * as Yup from 'yup';
 import { ErrorMessage, Formik, Form, Field } from "formik";
 import { master, visa } from "../assets";
 import { useSelector } from "react-redux";
